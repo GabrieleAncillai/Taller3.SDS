5e0e402e0a8cfa0ee38a9b5afbf7eb1aab29cfb4
Muffin-chan <42274306+GabrieleAncillai@users.noreply.github.com>
2020-06-28 21:09:28 -0500

    PUSH EQUISDE

--- BaseAPI.py
+++ BaseAPI.py
@@ -1,32 +1,33 @@
 from flask import Flask, jsonify, request
+import random
+import flask
 import os
 import copy
-import socket
 import uuid
-import json
+import socket
 
 S = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 S.connect(('8.8.8.8', 80))
 
 ExecuteHost = S.getsockname()[0]
-#ExecuteHost = 'tayer3simulacion3.tk'
 
 app = Flask(__name__)
-emp = [
+
+Empresas = [
     {
-        "name": 'IBM' , "posi": 3
+        "name": 'SaleForce.com' , "id": "1"
     },
     {
-        "name": 'SaleForce.com' , "posi": 4
+        "name": 'IBM' , "id": "2"
     },
     {
-        "name": 'SAP' , "posi": 5
+        "name": 'SAP' , "id": "3"
     },
     {
-        "name": 'Amazon' , "posi": 2
+        "name": 'Amazon' , "id": "4"
     },
     {
-        "name": 'Microsoft' , "posi": 1
+        "name": 'Microsoft' , "id": "5"
     }
 ]
 
@@ -36,18 +37,88 @@ def Hello():
     return {"Message": "Hello!"}
 
 
-@app.route("/empresas")
-def Empress():
+@app.route("/CrearEmpresa", methods=['POST']) #C
+def CrearEmpresa():
+    Enterprise = request.json
+    #Enterprise["id"] = uuid.uuid4()
+    Enterprise["id"] = str(random.randint(100, 999))
+    Empresas.append(Enterprise)
+    response = {
+        "data": Empresas,
+        "message": "Se agregó la empresa!"
+    }
+    try:
+        return response
+    except NameError:
+        return {
+            "message": str(NameError)
+        }
+
+    
+@app.route("/ListadoEmpresas") #R
+def ListadoEmpresas():
     return {
-        "data": emp
+        "data": Empresas
     }
 
+@app.route("/ActualizarEmpresa", methods=['POST']) #U
+def ActualizarEmpresa():
+    try:
+        Enterprise = request.json
+        didFind = False
+        response = {}
 
-@app.route("/empdest")
-def destacadas():
-    for x in (emp):
-        print (x)
-    return {}
+        for x in range(len(Empresas)):
+            if (str(Empresas[x].get('id')) == str(Enterprise.get('id'))):
+                Empresas[x] = Enterprise # El index en donde se encuentra la empresa, se edita
+                didFind = True
+
+        if not didFind:
+            response = {
+                "message": "No se encontró la empresa :("
+            }
+        else: 
+            response = {
+                "data": Empresas,
+                "message": "Se ha actualizado :)"
+            }
+        return response
+
+    except NameError:
+        return {
+            "message": str(NameError)
+        }
+
+
+    
+
+@app.route("/BorrarEmpresa", methods=['DELETE']) #D
+def BorrarEmpresa():
+    try:
+        Enterprise = request.json
+        didFind = False
+        response = {}
+
+        for x in range(len(Empresas)):
+            if (str(Empresas[x].get('id')) == str(Enterprise.get('id'))):
+                Empresas.pop(x) # El index en donde se encuentra la empresa, se borra
+                didFind = True
+
+        if not didFind:
+            response = {
+                "message": "No se encontró la empresa :("
+            }
+        else: 
+            response = {
+                "data": Empresas,
+                "message": "Se ha borrado xd :)"
+            }
+        return response
+
+    except NameError:
+        return {
+            "message": str(NameError)
+        }
 
 
 if __name__ == '__main__':
